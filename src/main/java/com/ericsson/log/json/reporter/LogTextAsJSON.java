b7b39ad7eca4cbb92bbc18cdbb844e3150daaa18
evijaka <vijayalakshmi.karthikeyan@ericsson.com>
2015-06-30 20:38:03 +0530

    pkg commit

---
+++ src/main/java/com/ericsson/log/json/reporter/LogTextAsJSON.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright Ericsson. This program is not to be copied or distributed
+ * without the express written consent of Ericsson. No part of this program
+ * may be used for purposes other than those intended by Ericsson.
+ * 
+ * Source: LogTextAsJSON.java
+ * Created by: evijaka
+ * Author: evijaka
+ * Date : Jun 30, 2015
+ */
+/**
+ * 
+ */
+package com.ericsson.log.json.reporter;
+
+
+/**
+ * @author evijaka
+ *
+ */
+public class LogTextAsJSON {
+
+}
